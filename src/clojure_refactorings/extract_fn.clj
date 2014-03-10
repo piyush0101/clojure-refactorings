08c95da2617af68f19ab6abeebc2eae541f53a97
Piyush Srivastava <piyush.0101@gmail.com>
2014-03-10 15:15:58 -0500

    initial commit

--- src/clojure_refactorings/extract_fn.clj
+++ src/clojure_refactorings/extract_fn.clj
@@ -2,19 +2,30 @@
 
 (defn extract-fn
   [body]
-  `(fn [] ~body))
+  `(~(symbol "fn") [] ~body))
 
 (defn extract-defn
   [name body]
   (if-not (seq? body)
     (throw (Exception. "not a valid lisp form"))
-  `(defn ~name [] ~body)))
+   (concat `(~(symbol "defn") ~name []) body)))
 
-(defn replace-with-fn-call
-  [func body]
-  `~func)
+(defn extract-defn-from-fn
+  [name body]
+  (if-not (seq? body)
+    (throw (Exception. "not a valid lisp form"))
+  (cons 'defn (cons name (drop 1 body)))))
 
-(defn extract-and-replace
+(defn extract-and-replace-defn
   [name body]
   {:fn (extract-defn name body)
    :call `(~name)})
+
+(defn extract-and-replace-anonymous-fn
+  [name body]
+  {:fn (extract-defn-from-fn name body)
+   :call (cons name (seq (nth body 1)))})
+
+(defn into-seq
+  [code-as-string]
+  (read-string (str "("code-as-string")")))
--- test/clojure_refactorings/t_extract_fn.clj
+++ test/clojure_refactorings/t_extract_fn.clj
@@ -4,39 +4,60 @@
 
 (facts "about `extract-fn`"
        (fact "it extracts an anonymous function (fn) given a body with a single form"
-             (extract-fn '(println "hello world")) => '(clojure.core/fn [] (println "hello world")))
+             (extract-fn '(println "hello world")) => '(fn [] (println "hello world")))
 
        (fact "it extracts an anonymous function (fn) given a body as a nested sequence of more than one form"
              (extract-fn '(if (nil? sequence)
                                 (print "nil")
-                               (print "not nil"))) => '(clojure.core/fn [] (if (nil? sequence) (print "nil") (print "not nil"))))
+                               (print "not nil"))) => '(fn [] (if (nil? sequence) (print "nil") (print "not nil"))))
 
        (fact "extracted function is a valid function that can be evaluated"
              ((eval (extract-fn '(+ 1 2)))) => 3))
 
 (facts "about `extract-defn`"
        (fact "it extracts a function (defn) with a given name given a single form"
-             (extract-defn 'display '(println "hello world")) => '(clojure.core/defn display [] (println "hello world")))
+             (extract-defn 'display '((println "hello world"))) => '(defn display [] (println "hello world")))
 
        (fact "it extracts an named function (defn) given a body as a nested sequence of more than one form"
-             (extract-defn 'conditional-print '(if (nil? sequence)
+             (extract-defn 'conditional-print '((if (nil? sequence)
                                 (print "nil")
-                               (print "not nil"))) =>
-             '(clojure.core/defn conditional-print [] (if (nil? sequence) (print "nil") (print "not nil"))))
+                               (print "not nil")))) =>
+             '(defn conditional-print [] (if (nil? sequence) (print "nil") (print "not nil"))))
 
-       (fact "it throws an exception when given an invalid lisp form"
-             (extract-defn 'display [+ 1 2]) => (throws Exception)))
-
-(facts "about `replace-with-fn-call`"
-       (fact "it replaces a body with a named function call with no arguments"
-             (replace-with-fn-call '(display) '(println "hello world")) => '(display)))
+       (fact "it extracts a named function (defn) given non nested lisp forms"
+             (extract-defn 'add-and-subtract '((+ 1 2) (- 1 2))) => '(defn add-and-subtract [] (+ 1 2) (- 1 2)))
+       )
 
 (facts "about `extract-and-replace`"
        (fact "it throws an exception if not given a name of the function"
-             (extract-and-replace '(+ 1 2)) => (throws Exception))
+             (extract-and-replace-defn '(+ 1 2)) => (throws Exception))
 
        (fact "it throws an exception if not given a form"
-             (extract-and-replace 'display) => (throws Exception))
+             (extract-and-replace-defn 'display) => (throws Exception))
 
        (fact "it returns a map containing extracted function and replacement call"
-             (extract-and-replace 'display '(+ 1 2)) => {:fn '(clojure.core/defn display [] (+ 1 2)) :call '(display)}))
+             (extract-and-replace-defn 'display '((+ 1 2))) => {:fn '(defn display [] (+ 1 2)) :call '(display)}))
+
+(facts "about `extract-defn-from-fn`"
+       (fact "it converts a fn with no arguments to a defn with a given name"
+             (extract-defn-from-fn 'add '(fn [] (+ 1 2))) => '(defn add [] (+ 1 2)))
+
+       (fact "it converts a fn with one argument to a defn with a given name and a single argument"
+             (extract-defn-from-fn 'display '(fn [data] (println data))) => '(defn display [data] (println data)))
+
+       (fact "it converts a fn with multiple arguments to a defn with a given name and multiple arguments"
+             (extract-defn-from-fn 'add '(fn [a b] (+ a b))) => '(defn add [a b] (+ a b))))
+
+(facts "about `extract-and-replace-anonymous-fn`"
+       (fact "it replaces anonymous fn with no arguments with function call"
+             (:call (extract-and-replace-anonymous-fn 'add '(fn [] (+ 1 2)))) => '(add))
+
+       (fact "it replaces anonymous fn with one argument with a function call"
+             (:call (extract-and-replace-anonymous-fn 'display '(fn [data] (println data)))) => '(display data))
+
+       (fact "it replaces anonymous fn with multiple arguments with a function call"
+             (:call (extract-and-replace-anonymous-fn 'add '(fn [a b] (+ a b)))) => '(add a b)))
+
+(facts "about `into-seq`"
+       (fact "it converts two non nested forms into a seq with two forms"
+             (into-seq "(+ 1 2) (- 1 2)") => '((+ 1 2) (- 1 2))))
